bee39618f690bfb68bb0523dd36208da5fff9542
James Teversham <jamestevers@gmail.com>
2019-05-12 17:01:06 +0200

    fixed GCP database host address

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 
 //create connection object to DB
 var con = mysql.createConnection({
-  host: "http://35.234.136.126",//"bps-instance.cl36mtjf67xy.eu-west-1.rds.amazonaws.com",
+  host: "35.234.136.126",//"bps-instance.cl36mtjf67xy.eu-west-1.rds.amazonaws.com",
   user: "root",
   password: "Imangi43052010",
   database: "bps_db"
